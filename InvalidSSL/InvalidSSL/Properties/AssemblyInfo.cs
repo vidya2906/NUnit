daa9687b7409627f1e02a50c80a9211590206514
vidya2906 <101714184+vidya2906@users.noreply.github.com>
2022-05-16 10:04:49 +0530

    Add files via upload

---
+++ InvalidSSL/InvalidSSL/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("InvalidSSL")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("InvalidSSL")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2b5d9a8f-58d5-4253-8067-5209a758c16b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ InvalidSSL/InvalidSSL/UnitTest1.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+
+namespace ExcelDataTesting
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        IWebDriver driver;
+        IJavaScriptExecutor jsEx;
+
+        public TestContext TestContext { get; set; }
+
+        [TestMethod]
+        public void HandelInvalidSSLCerticateError()
+        {
+            ChromeOptions chromeOptions = new ChromeOptions();
+            chromeOptions.AcceptInsecureCertificates = true;
+
+            driver = new ChromeDriver(chromeOptions);
+            jsEx = (IJavaScriptExecutor)driver;
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("https://expired.badssl.com/");
+            Thread.Sleep(9000);
+            driver.Close();
+            driver.Quit();
+        }
+    }
+}
