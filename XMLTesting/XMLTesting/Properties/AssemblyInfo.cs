cfb908fb338d2ab5c806886fbf2a5e07ebb562f4
vidya2906 <101714184+vidya2906@users.noreply.github.com>
2022-05-16 09:59:27 +0530

    Add files via upload

---
+++ XMLTesting/XMLTesting/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("XMLTesting")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("XMLTesting")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d6994ea7-9a2b-4d92-935f-10d91d6146c0")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ XMLTesting/XMLTesting/UnitTest1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace excel
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        IWebDriver Driver;
+        IJavaScriptExecutor js;
+        
+        public TestContext TestContext { get; set; }
+
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.XML","|DataDirectory|\\XMLData.xml","user",DataAccessMethod.Sequential)]
+        
+        [TestMethod]
+        public void ExcelData()
+        {
+            {
+                Driver = new ChromeDriver();
+                js = (IJavaScriptExecutor)Driver;
+                Driver.Manage().Window.Maximize();
+                Driver.Navigate().GoToUrl("https://demoqa.com/text-box");
+                Driver.FindElement(By.Id("userName")).SendKeys(TestContext.DataRow[0].ToString());
+                Driver.FindElement(By.Id("userEmail")).SendKeys(TestContext.DataRow[1].ToString());
+                Driver.FindElement(By.Id("currentAddress")).SendKeys(TestContext.DataRow[2].ToString());
+                Driver.FindElement(By.Id("permanentAddress")).SendKeys(TestContext.DataRow[3].ToString());
+
+                js.ExecuteScript("window.scrollBy(0,250)");
+
+                Thread.Sleep(2000);
+                Driver.FindElement(By.Id("submit")).Click();
+                Driver.Close();
+                Driver.Quit();
+
+            }
+        }
+    }
+}
